c1b0bcd9f0b8c2982ca51d2b07e1d5cfa50c4416
Anton Börnfelt <antonb@MacBook-Pro-som-tillhor-Anton.local>
2026-05-07 10:43:06 +0200

    merged points-counter onto develop

--- features/target-tap-game/components/gameConstants.tsx
+++ features/target-tap-game/components/gameConstants.tsx
@@ -1,5 +1,5 @@
 export const CIRCLE_SIZE = 15;
-export const CONTAINER_WIDTH = 100;
+export const CONTAINER_WIDTH = 300;
 export const CONTAINER_HEIGHT = 500;
 export const BORDER_WIDTH = 2; // border thickness on each side
 export const BUOYANCY = 0.08; // strength of center-seeking force
