4b5ec38f7c79e1b555624eeceef01c01a680e996
EmmaRask <emmarask@yahoo.com>
2026-05-07 22:44:17 +0200

    debugging test in app/page

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,11 +1,7 @@
 
-// import GameScreen from "@/features/target-tap-game/components/GameScreen";
-// import { StartScreen } from "@/features/target-tap-game/components/StartScreen";
-
-// export default function PlayPage() {
-//   return <GameScreen />;
-// }
+import GameScreen from "@/features/target-tap-game/components/GameScreen";
 
 export default function PlayPage() {
-  return <main>PLAY WORKS</main>;
+  return <GameScreen />;
 }
+
