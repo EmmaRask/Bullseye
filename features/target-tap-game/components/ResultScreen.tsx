6d47a4ce5a4a4e1ec085f554a60f1187cfd8dd26
EmmaRask <emmarask@yahoo.com>
2026-05-12 00:17:19 +0200

    updated design in leaderboard and startpage. changed code (more Sections, hero, classname) slightly to better work with design.

--- features/target-tap-game/components/ResultScreen.tsx
+++ features/target-tap-game/components/ResultScreen.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabase';
 import { useRouter } from 'next/navigation';
+import styles from './ResultScreen.module.css';
 
 export function ResultScreen() {
   const router = useRouter();
@@ -34,27 +35,52 @@ export function ResultScreen() {
     fetchScores();
   }, []);
 
-  return <>
-    <h1>High scores</h1>
-    <p>Your score: </p>
-    <div className='top10'>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <div key={i}>
-          #{i + 1} {scores[i]?.player_name || '-'} - {scores[i]?.score || '-'}
-        </div>
-      ))}
-    </div>
-    <div className='rewards'></div>
-    <button
-      className='replay'
-      onClick={() => router.push('/')}>
-      Replay
-    </button>
 
-    <button
-      className='quit'
-      onClick={() => router.push('/')}>
-      Quit
-    </button>
-  </>;
+return (
+    <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <section className={styles.titlePanel}>
+          <h1>Scoreboard</h1>
+        </section>
+
+        <section className={styles.scorePanel}>
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className={`${styles.scoreRow} ${
+                index === 4 ? styles.currentPlayerRow : ''
+              }`}
+            >
+              <span>
+                {index + 1}. {scores[index]?.player_name || '-'}
+              </span>
+              <span>{scores[index]?.score ?? '-'}</span>
+            </div>
+          ))}
+        </section>
+
+        <section className={styles.rewardPanel}>
+          <p>5€</p>
+          <div className={styles.stampCircle}></div>
+        </section>
+
+        <section className={styles.buttonPanel}>
+          <button
+            className={styles.playAgainButton}
+            onClick={() => router.push('/')}
+          >
+            Play again
+          </button>
+
+          <button
+            className={styles.exitButton}
+            onClick={() => router.push('/')}
+          >
+            Payout & Exit
+          </button>
+        </section>
+      </div>
+    </main>
+  );
 }
+
