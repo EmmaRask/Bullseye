9cb7fee445d61237cbfa0f20a5c85e3b0a261a1f
EmmaRask <emmarask@yahoo.com>
2026-05-26 13:57:29 +0200

    tutorial how to play

--- features/target-tap-game/components/StartScreen.tsx
+++ features/target-tap-game/components/StartScreen.tsx
@@ -5,6 +5,7 @@ import { useStartGame } from "../../../hooks/useStartGame";
 import { useTivoliUser } from "../../../hooks/useTivoliUser";
 import { GameSessionModal } from "./GameSessionModal";
 import styles from "./StartScreen.module.css";
+import { PAYOUT_AMOUNT, REPLAY_COST, WIN_LIMIT, ENTRY_COST } from "@/game/config";
 
 export function StartScreen() {
   const {
@@ -49,11 +50,38 @@ export function StartScreen() {
           </p>
       )}
         <h2>How to play</h2>
-        <p>
-          Mobile: Click on the screen to shoot at the targets throu your moving crosshair! 
-          <br/>
-          Use computer? no worries parner! Just use your spacebar on your computer to hit your targets, same moving crosshair will test your skills!
-        </p>
+        <div className={styles.instructions}>
+          <p>
+            {ENTRY_COST}€ to play game to win {PAYOUT_AMOUNT}€ and get your fancy stamp.
+          </p>
+
+          <p>
+            Winnings over {WIN_LIMIT} points: {PAYOUT_AMOUNT}€!
+          </p>
+
+          <p>
+            Each target gives different points:
+          </p>
+
+          <ul>
+            <li>XL - 10 points</li>
+            <li>L - 20 points</li>
+            <li>M - 30 points</li>
+            <li>S - 40 points</li>
+          </ul>
+
+          <p>
+            Replay cheap during your token period for just {REPLAY_COST}€!
+          </p>
+
+          <p>
+            Mobile: tap the screen to shoot through your moving crosshair.
+          </p>
+
+          <p>
+            Computer: use spacebar or tuchpad to hit your targets, partner!
+          </p>
+        </div>
       </section>
 
       <section className={styles.panel}>
